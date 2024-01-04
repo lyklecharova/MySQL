baca7acd54b2748568744d2adf3e1e9bb44f4b9c
lyklecharova <lklecharova9@gmail.com>
2024-01-04 20:52:31 +0200

    Update Basic CRUD.sql

--- Basic CRUD - Lab/Basic CRUD.sql
+++ Basic CRUD - Lab/Basic CRUD.sql
@@ -4,3 +4,12 @@ use hotel;
 SELECT id, first_name, last_name, job_title 
 FROM employees
 ORDER BY id;
+
+-- 02. Select Employees with Filter
+SELECT id, 
+CONCAT_WS(' ', first_name, last_name) AS 'full_name', 
+job_title, 
+salary
+FROM employees
+WHERE salary > 1000
+ORDER BY id;
