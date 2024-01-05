5ac12a3397d70a0fac9d8789ec3c95e166dbc66c
lyklecharova <lklecharova9@gmail.com>
2024-01-05 21:21:47 +0200

    Update CRUD - Exercise.sql

--- Basic CRUD - Exercise/CRUD - Exercise.sql
+++ Basic CRUD - Exercise/CRUD - Exercise.sql
@@ -36,3 +36,78 @@ FROM employees;
  FROM employees
  WHERE salary BETWEEN 20000 AND 30000
  ORDER BY employee_id;
+
+-- 9 Find Names of All Employees
+SELECT 
+	CONCAT_WS(' ' , first_name, middle_name, last_name) AS 'Full Name'
+FROM employees
+WHERE 
+	salary IN (25000, 14000, 12500, 23600);
+
+-- 10 Find All Employees Without Manager
+SELECT first_name, last_name
+FROM employees
+WHERE manager_id IS NULL;
+
+-- 11 Find All Employees with salary More Than 50000
+SELECT first_name, last_name, salary
+FROM employees
+WHERE salary > 50000
+ORDER BY salary DESC;
+
+-- 12 Find 5 Best Paid Employees
+SELECT first_name, last_name
+FROM employees
+ORDER BY salary DESC
+LIMIT 5;
+
+-- 13 Find All Employees Except Marketing
+SELECT first_name, last_name
+FROM employees
+WHERE department_id != 4;
+
+-- 14 Sort Employees Table
+SELECT *
+FROM employees
+ORDER BY 
+	salary DESC,
+    first_name ASC,
+    last_name DESC,
+    middle_name ASC;
+
+-- 15 Create View Employees with Salaries
+CREATE VIEW  v_employees_salaries AS
+	SELECT first_name, last_name, salary
+    FROM employees;
+
+-- 16 Create View Employees with Job Titles
+CREATE VIEW  v_employees_job_titles AS
+	SELECT
+		CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name,
+        job_title
+        FROM employees;
+
+-- 17 Distinct Job Titles
+SELECT DISTINCT job_title
+FROM employees
+ORDER BY job_title ASC;
+
+-- 18 Find First 10 Started Projects
+SELECT *
+FROM projects
+ORDER BY start_date ASC, name ASC
+LIMIT 10;
+
+-- 19 Last 7 Hired Employees
+SELECT first_name, last_name, hire_date
+FROM employees
+ORDER BY hire_date DESC
+LIMIT 7;
+
+-- 20 Increase Salaries
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id IN (1, 2, 4, 11);
+
+SELECT salary
+FROM employees;
