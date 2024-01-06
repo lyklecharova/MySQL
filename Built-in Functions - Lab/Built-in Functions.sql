6513a981e7d2db5fbb506e3c43f4d63c096b5db7
lyklecharova <lklecharova9@gmail.com>
2024-01-06 16:12:40 +0200

    Create Built-in Functions.sql

---
+++ Built-in Functions - Lab/Built-in Functions.sql
@@ -0,0 +1,32 @@
+use book_library;
+-- 1 Find Book Titles
+ SELECT title 
+	AS `title`
+ FROM books
+ WHERE SUBSTRING(title,1, 3) = 'The'
+ ORDER BY id;
+ 
+ -- 2 Replace Titles
+SELECT REPLACE(title, 'The', '***') 
+	AS `title`
+FROM books
+WHERE SUBSTRING(title, 1,3) = 'The'
+ORDER BY id;
+
+
+-- 3 Sum Cost of All Books 
+SELECT SUM(cost)
+FROM books;
+
+-- 4 Days Lived
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS 'Full Name',
+    DATEDIFF(died, born) AS 'Days Lived'
+FROM
+    authors;
+
+-- 5 Harry Potter Books
+SELECT title
+FROM books
+WHERE title LIKE '%Harry Potter%'
+ORDER BY id;
