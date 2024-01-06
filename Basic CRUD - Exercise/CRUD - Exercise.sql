b0b9fabc828a6fb58b5ea0b975d0e1edd9269134
lyklecharova <lklecharova9@gmail.com>
2024-01-06 07:59:42 +0200

    Update CRUD - Exercise.sql

--- Basic CRUD - Exercise/CRUD - Exercise.sql
+++ Basic CRUD - Exercise/CRUD - Exercise.sql
@@ -111,3 +111,35 @@ WHERE department_id IN (1, 2, 4, 11);
 
 SELECT salary
 FROM employees;
+
+use geography;
+
+-- 21 All Mountain Peaks
+SELECT peak_name
+FROM peaks
+ORDER BY peak_name ASC;
+
+-- 22 Biggest Countries by Population
+SELECT country_name, population
+FROM  countries
+WHERE  continent_code = 'EU'
+ORDER BY population DESC , country_name ASC
+LIMIT 30;
+
+-- 23 Countries and Currency (Euro / Not Euro)
+SELECT 
+    country_name,
+    country_code,
+    IF(`currency_code` = 'EUR',
+        'Euro',
+        'Not Euro') AS `currency`
+FROM
+    countries
+ORDER BY country_name ASC;
+
+use diablo;
+
+-- 24 All Diablo Characters
+SELECT name
+FROM characters
+ORDER BY name ASC;
