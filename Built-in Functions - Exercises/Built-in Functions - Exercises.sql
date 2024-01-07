05ac21b2861faa98192baedb8010bbb5d734f99a
lyklecharova <lklecharova9@gmail.com>
2024-01-07 13:07:05 +0200

    Update Built-in Functions - Exercises.sql

--- Built-in Functions - Exercises/Built-in Functions - Exercises.sql
+++ Built-in Functions - Exercises/Built-in Functions - Exercises.sql
@@ -27,3 +27,44 @@ FROM employees
 WHERE
 	LOWER(job_title) NOT LIKE '%engineer%'
 ORDER BY employee_id;
+
+-- 5 Find Towns with Name Length
+SELECT `name`
+FROM `towns`
+WHERE 
+	CHAR_LENGTH(`name`) IN (5,6)
+ORDER BY `name` ASC;
+
+-- 6 Find Towns Starting With
+SELECT `town_id`, `name`
+FROM `towns`
+WHERE
+	LOWER(`name`) LIKE 'm%'
+    OR LOWER(`name`) LIKE 'k%'
+    OR LOWER(`name`) LIKE 'b%'
+    OR LOWER(`name`) LIKE 'e%'
+ORDER BY `name`ASC;
+
+-- 7 Find Towns Not Starting With
+SELECT `town_id`, `name`
+FROM `towns`
+WHERE
+	/*  
+    LOWER(name) NOT LIKE 'r%'
+    AND LOWER(name) NOT LIKE 'b%'
+    AND LOWER(name) NOT LIKE 'd%'
+    */
+	`name` REGEXP '^[^RrBbDd]'
+ORDER BY `name`ASC;
+
+-- 8 Create View Employees Hired After 2000 Year
+CREATE VIEW v_employees_hired_after_2000 AS
+	SELECT first_name, last_name
+    FROM employees
+    WHERE YEAR(hire_date) > 2000;
+
+-- 9 Length of Last Name
+SELECT first_name, last_name
+FROM employees
+WHERE
+	LENGTH(`last_name`) = 5;
