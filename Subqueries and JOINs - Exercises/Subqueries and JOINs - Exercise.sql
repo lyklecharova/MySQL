5ffef4be40c14151f405035880938878d87e2a94
lyklecharova <lklecharova9@gmail.com>
2024-01-13 19:26:37 +0200

    Create Subqueries and JOINs - Exercise.sql

---
+++ Subqueries and JOINs - Exercises/Subqueries and JOINs - Exercise.sql
@@ -0,0 +1,87 @@
+USE `soft_uni`;
+
+-- 1 Employee Address
+SELECT 
+    `e`.`employee_id`,
+    `e`.`job_title`,
+    `a`.`address_id`,
+    `a`.`address_text`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+ORDER BY `a`.`address_id` ASC
+LIMIT 5;
+
+-- 2 Addresses with Towns
+SELECT 
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `t`.`name` AS 'town',
+    `a`.`address_text`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+        JOIN
+    `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+ORDER BY `e`.`first_name` , `e`.`last_name`
+LIMIT 5;
+
+-- 3 Sales Employee
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `d`.`name` AS 'department_name'
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+    `d`.`name` = 'Sales'
+ORDER BY `e`.`employee_id` DESC
+LIMIT 5
+
+-- 4 Employee Departments
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `e`.`salary`,
+    `d`.`name` AS 'department_name'
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+    `e`.`salary` > 15000
+ORDER BY `d`.`department_id` DESC
+LIMIT 5;
+
+-- 5 Employees Without Project
+SELECT 
+    `e`.`employee_id`, `e`.`first_name`
+FROM
+    `employees` AS `e`
+        LEFT JOIN
+    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+WHERE
+    `ep`.`employee_id` IS NULL
+ORDER BY `e`.`employee_id` DESC
+LIMIT 3;
+
+-- 6 Employees Hired After
+SELECT 
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `e`.`hire_date`,
+    `d`.`name` AS 'dept_name'
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+    `e`.`hire_date` > 1 / 1 / 1999
+        AND `d`.`name` = 'Sales'
+        OR `d`.`name` = 'Finance'
+ORDER BY `e`.`hire_date` ASC;
