cc5118111b5dd979d9968e852a65c5958ce96e79
lyklecharova <lklecharova9@gmail.com>
2024-01-21 09:41:03 +0200

    Update Database Programmability and Transactions - Exercises.sql
    Programmability and Transactions

--- Database Programmability and Transactions - Exercises/Database Programmability and Transactions - Exercises.sql
+++ Database Programmability and Transactions - Exercises/Database Programmability and Transactions - Exercises.sql
@@ -128,3 +128,20 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- 11 Calculating Interest
+DELIMITER $$
+CREATE PROCEDURE `usp_calculate_future_value_for_account` (`id_input` INT, `account_interest_rate` DECIMAL(19,4))
+BEGIN
+SELECT
+		`a`. `id`  AS 'account_id',
+		`ah`. `first_name`,
+        `ah`. `last_name`,
+        `a`. `balance` AS 'current_balance',
+        `ufn_calculate_future_value`(`a`. `balance`, `account_interest_rate`, 5) AS 'balance_in_5_years'
+	FROM `account_holders` AS `ah`
+		JOIN `accounts` AS `a` ON `ah`.`id` = `a`.`account_holder_id`
+	WHERE `a`. `id` = `id_input`;
+END $$
+DELIMITER ;
+CALL `usp_calculate_future_value_for_account`(1, 0.1);
+
