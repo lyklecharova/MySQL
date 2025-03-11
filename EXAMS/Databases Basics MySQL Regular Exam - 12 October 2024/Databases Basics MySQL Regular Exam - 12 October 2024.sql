b33e514c9babf250b5624845f1c95d1ae946da2e
lyklecharova <lklecharova9@gmail.com>
2025-03-11 08:18:20 +0200

    02 Insert: Insert athletes with updated first name, last name, age, and country_id from athletes and countries tables where country name starts with 'A'.
    03 Update: Remove the word 'weight' from discipline names in the disciplines table.
    
    04 Delete: Remove athletes older than 35 years from the athletes table.

--- EXAMS/Databases Basics MySQL Regular Exam - 12 October 2024/Databases Basics MySQL Regular Exam - 12 October 2024.sql
+++ EXAMS/Databases Basics MySQL Regular Exam - 12 October 2024/Databases Basics MySQL Regular Exam - 12 October 2024.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE `summer_olympics`;
 
+USE `summer_olympics`;
+
 -- countries
 CREATE TABLE
     `countries` (
@@ -52,3 +54,29 @@ CREATE TABLE
         CONSTRAINT `fk_disciplines_athletes_medals_medals` FOREIGN KEY (`medal_id`) REFERENCES `medals` (`id`),
         CONSTRAINT `unique_discipline_medal` UNIQUE (`discipline_id`, `medal_id`)
     );
+
+-- 02 Insert
+INSERT INTO
+    athletes (`first_name`, `last_name`, `age`, `country_id`)
+SELECT
+    UPPER(`a`.`first_name`),
+    CONCAT (`a`.`last_name`, ' comes from ', `c`.`name`),
+    `a`.`age` + `a`.`country_id`,
+    `a`.`country_id`
+FROM
+    `athletes` AS `a`
+    JOIN `countries` AS `c` ON `a`.`country_id` = `c`.`id`
+WHERE
+    `c`.`name` LIKE "A%";
+
+-- 03 Update
+UPDATE `disciplines`
+SET
+    `name` = REPLACE (`name`, "weight", "")
+WHERE
+    LOWER(`name`) LIKE "%weight%";
+
+-- 04 DELETE
+DELETE FROM `athletes`
+WHERE
+    `age` > 35;
